1e63aa44346ac5416d31ebf747ef3aed4391c4cb
Luke Parham <luke.parham@ovenbits.com>
2015-10-13 21:46:44 -0500

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Luke Parham on 10/13/15.
+//  Copyright © 2015 Luke Parham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
